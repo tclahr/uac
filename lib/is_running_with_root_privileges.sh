e1e7b54b9c86c008a64c6b0303d77ba3846f6b8c
Thiago Canozzo Lahr <tclahr@br.ibm.com>
2022-01-23 08:29:42 -0300

    fix: fixed issue related to ESXi
    Fix to issue that was preventing UAC to run on VMWare ESXi systems. ESXi does not have 'id' command, so get_current_user and /etc/passwd will be used instead.
    
    Signed-off-by: Thiago Canozzo Lahr <tclahr@br.ibm.com>

--- lib/is_running_with_root_privileges.sh
+++ lib/is_running_with_root_privileges.sh
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 ###############################################################################
-# Check if running uid is 0 (root privileges).
+# Check if the current user has root privileges.
 # Globals:
 #   None
 # Requires:
-#   None
+#   get_current_user
 # Arguments:
 #   None
 # Outputs:
@@ -28,10 +28,23 @@
 ###############################################################################
 is_running_with_root_privileges()
 {
-  ir_uid=`id -u`
 
-  if [ "${ir_uid}" = "0" ]; then
-    return 0 
+  ir_current_user=`get_current_user`
+  ir_uid=""
+
+  if [ "${ir_current_user}" = "root" ]; then
+    return 0
+  else
+    # id command is not available on VMWare ESXi
+    if eval "id -u" >/dev/null 2>/dev/null; then
+      ir_uid=`id -u`
+    elif [ -f "/etc/passwd" ]; then
+      ir_uid=`grep "^${ir_current_user}" /etc/passwd 2>/dev/null \
+                | awk 'BEGIN { FS=":"; } { print $3; }' 2>/dev/null`
+    fi
+    if [ "${ir_uid}" = "0" ]; then
+      return 0 
+    fi
   fi
   return 2
 
