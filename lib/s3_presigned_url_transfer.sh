53f6a3a3daafac4011256636a7cc5f70dc97907a
Thiago Canozzo Lahr <tclahr@br.ibm.com>
2022-03-25 07:46:54 -0300

    feat: transfer file to s3 presigned url
    Signed-off-by: Thiago Canozzo Lahr <tclahr@br.ibm.com>

---
+++ lib/s3_presigned_url_transfer.sh
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###############################################################################
+# Transfer file to S3 presigned URL.
+# Globals:
+#   None
+# Requires:
+#   None
+# Arguments:
+#   $1: source file
+#   $2: S3 presigned URL
+# Outputs:
+#   None.
+# Exit Status:
+#   Exit with status 0 on success.
+#   Exit with status greater than 0 if errors occur.
+###############################################################################
+s3_presigned_url_transfer()
+{
+  pu_source="${1:-}"
+  pu_s3_presigned_url="${2:-}"
+
+  curl \
+    --fail \
+    --request PUT \
+    --header "Content-Type: application/text" \
+    --header "Accept: */*" \
+    --header "Expect: 100-continue" \
+    --upload-file "${pu_source}" \
+    "${pu_s3_presigned_url}"
+
+}
