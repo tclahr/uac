30cee880562a1284548c6a01d968dd15f03a4b9b
Thiago Canozzo Lahr <tclahr@br.ibm.com>
2022-03-25 07:45:11 -0300

    feat: test sftp connectivity
    new function that tests connectivity to sftp server.
    
    Signed-off-by: Thiago Canozzo Lahr <tclahr@br.ibm.com>

---
+++ lib/sftp_transfer_test.sh
@@ -0,0 +1,54 @@
+# Copyright (C) 2020 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the “License”);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###############################################################################
+# Test the connectivity to SFTP server.
+# Globals:
+#   None
+# Requires:
+#   None
+# Arguments:
+#   $1: remote destination
+#   $2: remote port (default: 22)
+#   $3: identity file
+# Outputs:
+#   None.
+# Exit Status:
+#   Exit with status 0 on success.
+#   Exit with status greater than 0 if errors occur.
+###############################################################################
+sftp_transfer_test()
+{
+  sf_destination="${1:-}"
+  sf_port="${2:-22}"
+  sf_identity_file="${3:-}"
+
+  if [ -n "${sf_identity_file}" ]; then
+    sftp -P "${sf_port}" \
+      -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      -i "${sf_identity_file}" \
+      "${sf_destination}" >/dev/null << EOF
+pwd
+EOF
+  else
+    sftp -P "${sf_port}" \
+      -o StrictHostKeyChecking=no \
+      -o UserKnownHostsFile=/dev/null \
+      "${sf_destination}" >/dev/null << EOF
+pwd
+EOF
+  fi 
+
+}
