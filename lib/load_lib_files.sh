91af220a5c72c0521a8b4f264ffe8eebcf04c088
Thiago Canozzo Lahr <tclahr@br.ibm.com>
2023-01-15 15:35:48 -0300

    refactor: add SPDX-License-Identifier: Apache-2.0
    Signed-off-by: Thiago Canozzo Lahr <tclahr@br.ibm.com>

--- lib/load_lib_files.sh
+++ lib/load_lib_files.sh
@@ -1,36 +1,7 @@
 #!/bin/sh
-
-# Copyright (C) 2020 IBM Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the “License”);
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an “AS IS” BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# SPDX-License-Identifier: Apache-2.0
 # shellcheck disable=SC1091
 
-###############################################################################
-# Load library files required by UAC.
-# Globals:
-#   UAC_DIR
-# Requires:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-# Exit Status:
-#   Exit with status 0 on success.
-#   Exit with status greater than 0 if errors occur.
-###############################################################################
-  
 . "${UAC_DIR}/lib/archive_compress_data.sh"
 . "${UAC_DIR}/lib/archive_data.sh"
 . "${UAC_DIR}/lib/array_to_list.sh"
