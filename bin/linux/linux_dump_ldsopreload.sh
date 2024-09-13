04d85b9073b58e5f8a6df2f7275277b910cc18bc
Minoru Kobayashi <unknownbit@gmail.com>
2024-09-13 10:25:35 +0900

    refactor: tidy up linux_dump_ldsopreload.sh
    Refactor command options.

--- bin/linux/linux_dump_ldsopreload.sh
+++ bin/linux/linux_dump_ldsopreload.sh
@@ -18,14 +18,24 @@
 
 usage() {
     cat <<"EOM"
-Dump /etc/ld.so.preload (first sector only) via xfs_db
+Dump /etc/ld.so.preload (first sector only) with xfs_db
 
 Usage: $0 [-d dumpdir]
 
-    -d dumpdir   Specify the output directory
-    -v device    Specify the device which has /etc directory
+    -d device         Specify the device which has /etc directory
+    -h                Show this help message
+    -l sector_count   Specify the sector count to dump (default: 1)
+    -o outputfile     Specify the output file
+    -v                Enable verbose mode
 EOM
     exit 1;
+}
+
+
+print_msg() {
+    if [ ${verbose_mode} -eq 1 ]; then
+        echo $1
+    fi
 }
 
 
@@ -57,11 +67,19 @@ find_ldsopreload_inumber() {
 }
 
 
-while getopts "d:v:" opts; do
+sector_count=1
+verbose_mode=0
+while getopts "d:hl:o:v" opts; do
     case ${opts} in
-        d) outputdir=${OPTARG}
+        d) etc_dev=${OPTARG}
            ;;
-        v) etc_dev=${OPTARG}
+        h) usage
+           ;;
+        l) sector_count=${OPTARG}
+           ;;
+        o) outputfile=${OPTARG}
+           ;;
+        v) verbose_mode=1
            ;;
         *) usage
            ;;
@@ -72,52 +90,48 @@ done
 if [ -z "${etc_dev}" ]; then
     etc_dev=`df -T /etc | awk '$2 == "xfs" {print $1}'`
     if [ -z "${etc_dev}" ]; then
-        echo "/etc is not on XFS filesystem."
-        exit 1
+        print_msg "/etc is not on XFS filesystem."
+        exit 2
     fi
 else
-    if [ `df -T "${etc_dev}" | awk '$2 != "Type" {print $2}'` != "xfs" ]; then
-        echo "${etc_dev} is not XFS filesystem."
-        exit 1
+    if [ `df -T "${etc_dev}" | awk 'NR==2 {print $2}'` != "xfs" ]; then
+        print_msg "${etc_dev} is not XFS filesystem."
+        exit 2
     fi
 fi
-# echo "etc_dev: ${etc_dev}"
 
 # Get inode number of /etc directory itself.
 etc_inumber=`ls -id /etc | awk '{print $1}'`
-# echo "etc_inumber: ${etc_inumber}"
 
 # Get fsblock numbers of /etc directory.
 etc_fsblocks=`xfs_db -r ${etc_dev} -c "inode ${etc_inumber}" -c "bmap" | awk '{print $5}'`
-# echo "etc_fsblocks: ${etc_fsblocks}"
 
 # Find inode number of /etc/ld.so.preload file.
 for etc_fsblock in ${etc_fsblocks}; do
     etc_daddr=`xfs_db -r ${etc_dev} -c "convert fsblock ${etc_fsblock} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p'`
-    # echo "etc_daddr: ${etc_daddr}"
     ldsopreload_inumber=`find_ldsopreload_inumber ${etc_dev} ${etc_daddr}`
-    # echo "ldsopreload_inumber: ${ldsopreload_inumber}"
     if [ ${ldsopreload_inumber} -ne 0 ]; then
         break
     fi
 done
 
 if [ ${ldsopreload_inumber} -eq 0 ]; then
-    echo "/etc/ld.so.preload not found."
-    exit 1
+    print_msg "/etc/ld.so.preload not found."
+    exit 3
 fi
 
 # Get fsblock numbers of /etc/ld.so.preload file.
 # In many cases, there is only one fsblock.
 ldsopreload_fsblocks=`xfs_db -r ${etc_dev} -c "inode ${ldsopreload_inumber}" -c "bmap" | awk '{print $5}'`
-# echo "ldsopreload_fsblocks: ${ldsopreload_fsblocks}"
 
 # Convert fsblock numbers to daddr.
 ldsopreload_daddr=`xfs_db -r ${etc_dev} -c "convert fsblock ${ldsopreload_fsblocks} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p'`
-# echo "ldsopreload_daddr: ${ldsopreload_daddr}"
 
 # Dump /etc/ld.so.preload file.
 # I believe that /etc/ld.so.preload is not so large.
 sector_size=`xfs_db -r /dev/mapper/rl-root -c "sb 0" -c "print" | grep -E "^sectsize" | awk '{print $3}'`
-# echo "sector_size: ${sector_size}"
-dd if=${etc_dev} bs=${sector_size} skip=${ldsopreload_daddr} count=1
+if [ -z "${outputfile}" ]; then
+    dd if="${etc_dev}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count}"
+else
+    dd if="${etc_dev}" of="${outputfile}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count}"
+fi
