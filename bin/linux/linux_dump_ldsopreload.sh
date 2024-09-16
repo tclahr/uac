125e3b88ce8ca7bdd416aa70e80dc080e37d09f3
Minoru Kobayashi <unknownbit@gmail.com>
2024-09-16 21:55:51 +0900

    artif: merge artifacts
    Merge debugfs.yaml and xfs_db.yaml into one file.

--- bin/linux/linux_dump_ldsopreload.sh
+++ bin/linux/linux_dump_ldsopreload.sh
@@ -18,13 +18,13 @@
 
 usage() {
     cat <<"EOM"
-Dump /etc/ld.so.preload (first sector only) with xfs_db
+Dump /etc/ld.so.preload on XFS or EXT-based filesystem.
 
-Usage: $0 [-d dumpdir]
+Usage: $0 [-d device] [-h] [-l sector_count] [-o outputfile] [-v]
 
     -d device         Specify the device which has /etc directory
     -h                Show this help message
-    -l sector_count   Specify the sector count to dump (default: 1)
+    -l sector_count   Specify the sector count to dump (only for XFS, default: 1)
     -o outputfile     Specify the output file
     -v                Enable verbose mode
 EOM
@@ -39,7 +39,7 @@ print_msg() {
 }
 
 
-find_ldsopreload_inumber() {
+find_xfs_ldsopreload_inumber() {
     xfs_db -r $1 -c "daddr $2" -c "type dir3" -c "print" | awk '
     {
         if ($0 ~ /du\[([0-9]+)\].inumber = ([0-9]+)/) {
@@ -67,6 +67,60 @@ find_ldsopreload_inumber() {
 }
 
 
+dump_xfs_ldsopreload() {
+    etc_dev_l=$1
+    outputfile_l=$2
+    sector_count_l=$3
+    # Get inode number of /etc directory itself.
+    etc_inumber=$(ls -id /etc | awk '{print $1}')
+
+    # Get fsblock numbers of /etc directory.
+    etc_fsblocks=$(xfs_db -r ${etc_dev_l} -c "inode ${etc_inumber}" -c "bmap" | awk '{print $5}')
+
+    # Find inode number of /etc/ld.so.preload file.
+    for etc_fsblock in ${etc_fsblocks}; do
+        etc_daddr=$(xfs_db -r ${etc_dev_l} -c "convert fsblock ${etc_fsblock} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p')
+        ldsopreload_inumber=$(find_xfs_ldsopreload_inumber ${etc_dev_l} ${etc_daddr})
+        if [ ${ldsopreload_inumber} -ne 0 ]; then
+            break
+        fi
+    done
+
+    if [ ${ldsopreload_inumber} -eq 0 ]; then
+        print_msg "/etc/ld.so.preload not found."
+        exit 3
+    fi
+
+    # Get fsblock numbers of /etc/ld.so.preload file.
+    # In many cases, there is only one fsblock.
+    ldsopreload_fsblocks=$(xfs_db -r ${etc_dev_l} -c "inode ${ldsopreload_inumber}" -c "bmap" | awk '{print $5}')
+
+    # Convert fsblock numbers to daddr.
+    ldsopreload_daddr=$(xfs_db -r ${etc_dev_l} -c "convert fsblock ${ldsopreload_fsblocks} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p')
+
+    # Dump /etc/ld.so.preload file.
+    # I believe that /etc/ld.so.preload is not so large.
+    sector_size=$(xfs_db -r /dev/mapper/rl-root -c "sb 0" -c "print" | grep -E "^sectsize" | awk '{print $3}')
+    if [ -z "${outputfile_l}" ]; then
+        dd if="${etc_dev_l}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count_l}" status=none
+    else
+        dd if="${etc_dev_l}" of="${outputfile_l}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count_l}" status=none
+    fi
+}
+
+
+dump_ext_ldsopreload() {
+    etc_dev_l=$1
+    outputfile_l=$2
+    # debugfs -R 'cat /etc/ld.so.preload' "${etc_dev_l}"
+    if [ -z "${outputfile_l}" ]; then
+        debugfs -R 'cat /etc/ld.so.preload' "${etc_dev_l}"
+    else
+        debugfs -R "dump /etc/ld.so.preload \"${outputfile_l}\"" "${etc_dev_l}"
+    fi
+}
+
+
 sector_count=1
 verbose_mode=0
 while getopts "d:hl:o:v" opts; do
@@ -88,50 +142,17 @@ done
 
 # Which device has /etc directory?
 if [ -z "${etc_dev}" ]; then
-    etc_dev=`df -T /etc | awk '$2 == "xfs" {print $1}'`
-    if [ -z "${etc_dev}" ]; then
-        print_msg "/etc is not on XFS filesystem."
-        exit 2
-    fi
+    read etc_dev fs_type <<< $(df -T /etc | awk 'NR==2 {print $1, $2}')
 else
-    if [ `df -T "${etc_dev}" | awk 'NR==2 {print $2}'` != "xfs" ]; then
-        print_msg "${etc_dev} is not XFS filesystem."
-        exit 2
-    fi
+    fs_type=$(df -T "${etc_dev}" | awk 'NR==2 {print $2}')
 fi
 
-# Get inode number of /etc directory itself.
-etc_inumber=`ls -id /etc | awk '{print $1}'`
-
-# Get fsblock numbers of /etc directory.
-etc_fsblocks=`xfs_db -r ${etc_dev} -c "inode ${etc_inumber}" -c "bmap" | awk '{print $5}'`
-
-# Find inode number of /etc/ld.so.preload file.
-for etc_fsblock in ${etc_fsblocks}; do
-    etc_daddr=`xfs_db -r ${etc_dev} -c "convert fsblock ${etc_fsblock} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p'`
-    ldsopreload_inumber=`find_ldsopreload_inumber ${etc_dev} ${etc_daddr}`
-    if [ ${ldsopreload_inumber} -ne 0 ]; then
-        break
-    fi
-done
-
-if [ ${ldsopreload_inumber} -eq 0 ]; then
-    print_msg "/etc/ld.so.preload not found."
-    exit 3
-fi
-
-# Get fsblock numbers of /etc/ld.so.preload file.
-# In many cases, there is only one fsblock.
-ldsopreload_fsblocks=`xfs_db -r ${etc_dev} -c "inode ${ldsopreload_inumber}" -c "bmap" | awk '{print $5}'`
-
-# Convert fsblock numbers to daddr.
-ldsopreload_daddr=`xfs_db -r ${etc_dev} -c "convert fsblock ${ldsopreload_fsblocks} daddr" | sed -n 's/.*(\([0-9]*\)).*/\1/p'`
-
-# Dump /etc/ld.so.preload file.
-# I believe that /etc/ld.so.preload is not so large.
-sector_size=`xfs_db -r /dev/mapper/rl-root -c "sb 0" -c "print" | grep -E "^sectsize" | awk '{print $3}'`
-if [ -z "${outputfile}" ]; then
-    dd if="${etc_dev}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count}" status=none
+# Check filesystem type and dump /etc/ld.so.preload
+if [ "${fs_type}" = "xfs" ]; then
+    dump_xfs_ldsopreload "${etc_dev}" "${outputfile}" "${sector_count}"
+elif [[ "${fs_type}" =~ ^ext ]]; then
+    dump_ext_ldsopreload "${etc_dev}" "${outputfile}"
 else
-    dd if="${etc_dev}" of="${outputfile}" bs="${sector_size}" skip="${ldsopreload_daddr}" count="${sector_count}" status=none
+    print_msg "/etc is not on XFS or EXT filesystem."
+    exit 2
 fi
